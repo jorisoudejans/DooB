b9c0c0a5a52594933a67eb7ae7c9707bde333ffe
cbilstra <cbilstra@student.tudeft.nl>
2015-09-03 12:10:00 +0200

    Edit checkstyle for getter/setter

--- src/main/java/doob/model/Game.java
+++ src/main/java/doob/model/Game.java
@@ -2,12 +2,23 @@ package doob.model;
 
 import java.util.ArrayList;
 
+/**
+ * Abstract class which is extended by SinglePlayerGame and MultiPlayerGame.
+ * @author Cas
+ *
+ */
 public abstract class Game {
 	
 	private int score;
 	private int difficulty;
 	private ArrayList<Level> levels;
 	
+	/**
+	 * Simple constructor.
+	 * @param score The current score
+	 * @param difficulty An integer which describes the difficulty of the game
+	 * @param levels The list of available levels for the game
+	 */
 	public Game(int score, int difficulty, ArrayList<Level> levels) {
 		this.score = score;
 		this.difficulty = difficulty;
