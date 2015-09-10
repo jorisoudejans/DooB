6b411f98da9a5e663ab0f5d18ef6544e189bc286
Hidde Lycklama <hidde@vlinderstorm.com>
2015-09-10 21:03:38 +0200

    Add start of smoketest Edit y

--- src/test/java/doob/AppSmokeTest.java
+++ src/test/java/doob/AppSmokeTest.java
@@ -1,6 +1,8 @@
 package doob;
 
+import javafx.application.Application;
 import junit.framework.TestCase;
+import org.junit.Test;
 
 /**
  * Created by hidde on 9/8/15.
@@ -9,6 +11,17 @@ import junit.framework.TestCase;
  */
 public class AppSmokeTest {
 
+    @Test
+    public void testSmokeTest() throws Exception {
+
+        Thread t = new Thread("JavaFX Init Thread") {
+            public void run() {
+                Application.launch(App.class, new String[0]);
 
 
+            }
+        };
+        t.start();
+        Thread.sleep(4000);
+    }
 }
