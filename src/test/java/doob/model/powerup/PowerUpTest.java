1fa768737112734808ce0da1489a23291d8750a9
Hidde Lycklama <hidde@vlinderstorm.com>
2015-09-10 23:14:07 +0200

    Add powerups and tests

---
+++ src/test/java/doob/model/powerup/PowerUpTest.java
@@ -0,0 +1,14 @@
+package doob.model.powerup;
+
+import doob.model.powerup.PowerUp;
+import org.junit.Test;
+
+/**
+ * Tests abstract class PowerUp
+ *
+ * Created by hidde on 9/10/15.
+ */
+public class PowerUpTest {
+
+    // nothing to test an abstract class for..
+}
