9e0d58f217f19658b2e95b3c471013e5c6afc6e0
Shane Koppers <shanekoppers@hotmail.com>
2015-09-11 20:01:59 +0200

    Refactored LevelFactory and added javadoc

--- src/main/java/doob/model/LevelFactory.java
+++ src/main/java/doob/model/LevelFactory.java
@@ -17,6 +17,12 @@ public class LevelFactory {
     private ArrayList<Ball> ballList;
     private Image[] playerImages;
 
+    /**
+     * Creates a LevelFactory object which all that is needed to build a level
+     *
+     * @param path path to the XML-file
+     * @param canvas javaFX canvas
+     */
     public LevelFactory(String path, Canvas canvas){
         this.file = new File(path);
         this.canvas = canvas;
@@ -30,9 +36,83 @@ public class LevelFactory {
         this.playerImages = images;
     }
 
-    public Level build() {
+    /**
+     * parses an int out of an xml line
+     *
+     * @param s The XML-tag to parse from
+     * @param eElement The XML-Element to parse from
+     * @return The parsed int
+     */
+    public int parseInt(String s, Element eElement){
+        return Integer.parseInt(eElement.getElementsByTagName(s)
+                .item(0)
+                .getTextContent());
 
-        //XML parsing
+    }
+
+    /**
+     * Parses and adds players in the XML to the playerList
+     *
+     * @param doc Document build from the XML
+     */
+    public void parsePlayers(Document doc){
+        NodeList nListPlayer = doc.getElementsByTagName("player");
+        for(int i = 0; i < nListPlayer.getLength(); i++){
+            Node nNode = nListPlayer.item(i);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                int x = parseInt("x", eElement);
+                int y = parseInt("y", eElement);
+                int width = parseInt("width", eElement);
+                int height = parseInt("height", eElement);
+
+                if (playerImages.length < 3) {
+                    playerImages = new Image[] { new Image("/image/character1_stand.png"),
+                            new Image("/image/character1_left.gif"),
+                            new Image("/image/character1_right.gif") };
+                }
+
+                Player pl = new Player(
+                        x,
+                        y,
+                        width,
+                        height,
+                        playerImages[0],
+                        playerImages[1],
+                        playerImages[2]
+                );
+                playerList.add(pl);
+            }
+        }
+    }
+
+    /**
+     * Parses and adds balls in the XML to the ballList
+     *
+     * @param doc Document build from the XML
+     */
+    public void parseBalls(Document doc){
+        NodeList nListBall = doc.getElementsByTagName("ball");
+        for(int i = 0; i < nListBall.getLength(); i++){
+            Node nNode = nListBall.item(i);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                int x = parseInt("x", eElement);
+                int y = parseInt("y", eElement);
+                int speedX = parseInt("speedX", eElement);
+                int speedY = parseInt("speedY", eElement);
+                int size = parseInt("size", eElement);
+
+                Ball ball = new Ball(x, y, speedX, speedY, size);
+                ballList.add(ball);
+            }
+        }
+    }
+
+    /**
+     * Parses the XML and adds all parsed objects to their respective lists
+     */
+    public void parseXML(){
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = factory.newDocumentBuilder();
@@ -40,74 +120,23 @@ public class LevelFactory {
             Document doc = dBuilder.parse(file);
             doc.getDocumentElement().normalize();
 
-            //Loop through all players and add them to the playerList
-            NodeList nListPlayer = doc.getElementsByTagName("player");
-            for(int i = 0; i < nListPlayer.getLength(); i++){
-                Node nNode = nListPlayer.item(i);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    int x = Integer.parseInt(eElement.getElementsByTagName("x")
-                            .item(0)
-                            .getTextContent());
-                    int y = Integer.parseInt(eElement.getElementsByTagName("y")
-                            .item(0)
-                            .getTextContent());
-                    int width = Integer.parseInt(eElement.getElementsByTagName("width")
-                            .item(0)
-                            .getTextContent());
-                    int height = Integer.parseInt(eElement.getElementsByTagName("height")
-                            .item(0)
-                            .getTextContent());
-
-                    if (playerImages.length < 3) {
-                        playerImages = new Image[] { new Image("/image/character1_stand.png"),
-                                new Image("/image/character1_left.gif"),
-                                new Image("/image/character1_right.gif") };
-                    }
-
-                    Player pl = new Player(
-                            x,
-                            y,
-                            width,
-                            height,
-                            playerImages[0],
-                            playerImages[1],
-                            playerImages[2]
-                    );
-                    playerList.add(pl);
-                }
-            }
-
-            //Loop through all balls and add them to the ballList
-            NodeList nListBall = doc.getElementsByTagName("ball");
-            for(int i = 0; i < nListBall.getLength(); i++){
-                Node nNode = nListBall.item(i);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    int x = Integer.parseInt(eElement.getElementsByTagName("x")
-                            .item(0)
-                            .getTextContent());
-                    int y = Integer.parseInt(eElement.getElementsByTagName("y")
-                            .item(0)
-                            .getTextContent());
-                    int speedX = Integer.parseInt(eElement.getElementsByTagName("speedX")
-                            .item(0)
-                            .getTextContent());
-                    int speedY = Integer.parseInt(eElement.getElementsByTagName("speedY")
-                            .item(0)
-                            .getTextContent());
-                    int size = Integer.parseInt(eElement.getElementsByTagName("size")
-                            .item(0)
-                            .getTextContent());
-
-                    Ball ball = new Ball(x, y, speedX, speedY, size);
-                    ballList.add(ball);
-                }
-            }
+            parsePlayers(doc);
+            parseBalls(doc);
 
         } catch(Exception e){
             e.printStackTrace();
         }
+    }
+
+
+    /**
+     * Builds the level from the parsed XML
+     * @return The build level
+     */
+    public Level build() {
+
+        //XML parsing
+        parseXML();
 
         Level.Builder builder = new Level.Builder();
         builder.setBalls(ballList);
