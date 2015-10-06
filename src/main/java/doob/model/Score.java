e34a3de2ce8497c25c0fd512c23707a3660cdb47
Jasper van Tilburg <J.F.vantilburg@student.tudelft.nl>
2015-10-06 15:19:08 +0200

    Added class HighscoreController along with test class <feature>

--- src/main/java/doob/model/Score.java
+++ src/main/java/doob/model/Score.java
@@ -5,25 +5,55 @@ package doob.model;
  */
 public class Score {
 
-    String name;
-    int score;
+    private String name;
+    private int score;
 
     /**
      *
      * @param name The name of the player
      * @param score The score of the player
      */
-    public Score(String name, int score){
+    public Score(String name, int score) {
         this.name = name;
         this.score = score;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public int getScore(){
+    public int getScore() {
         return this.score;
 
     }
+
+    /**
+     * Checks if two Scores are equal. 
+     * Used for testing reasons.
+     * @param obj The object the compare.
+     * @return wheter they are equal or not.
+     */
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Score other = (Score) obj;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (score != other.score) {
+			return false;
+		}
+		return true;
+	}
 }
---
+++ src/test/java/doob/controller/HighScoreControllerTest.java
@@ -0,0 +1,77 @@
+package doob.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import doob.model.Score;
+
+/**
+ *	Testclass for HighscoreController.
+ */
+public class HighScoreControllerTest {
+
+	/**
+	 * Test the read method.
+	 */
+	@Test
+	public void readTest() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest1.xml");
+		ArrayList<Score> expected = new ArrayList<Score>();
+		for (int i = 9; i >= 0; i--) {
+			expected.add(new Score(i + "nd", i));
+		}
+		ArrayList<Score> actual = hsc.read();
+		for (int i = 0; i < 10; i++) {
+			assertEquals(expected.get(i), actual.get(i));
+		}
+	}
+
+	/**
+	 * Test the highScoreIndex method with highest score.
+	 */
+	@Test
+	public void highScoreIndexTest1() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest2.xml");
+		hsc.read();
+		assertEquals(0, hsc.highScoreIndex(13));
+	}
+
+	/**
+	 * Test the highScoreIndex method with middle score.
+	 */
+	@Test
+	public void highScoreIndexTest2() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest2.xml");
+		hsc.read();
+		assertEquals(6, hsc.highScoreIndex(6));
+	}
+
+	/**
+	 * Test the highScoreIndex method with lowest score.
+	 */
+	@Test
+	public void highScoreIndexTest3() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest2.xml");
+		hsc.read();
+		assertEquals(9, hsc.highScoreIndex(2));
+	}
+
+	/**
+	 * Test the highScoreIndex method with a too low score.
+	 */
+	@Test
+	public void highScoreIndexTest4() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest2.xml");
+		hsc.read();
+		assertEquals(-1, hsc.highScoreIndex(1));
+	}
+
+}
