4b3b7ef3d81cdd3e0d46d4cef7c2787fa6a163a5
Jasper van Tilburg <J.F.vantilburg@student.tudelft.nl>
2015-10-08 01:41:27 +0200

    Added test suites for highscorecontroller <testing>

--- src/test/java/doob/controller/HighScoreControllerTest.java
+++ src/test/java/doob/controller/HighScoreControllerTest.java
@@ -25,9 +25,22 @@ public class HighScoreControllerTest {
 			expected.add(new Score(i + "nd", i));
 		}
 		ArrayList<Score> actual = hsc.read();
-		for (int i = 0; i < 10; i++) {
-			assertEquals(expected.get(i), actual.get(i));
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void writeTest() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest3.xml");
+		ArrayList<Score> expected = new ArrayList<Score>();
+		for (int i = 9; i >= 0; i--) {
+			expected.add(new Score(i + "nd", i));
 		}
+		hsc.setHighscores(expected);
+		hsc.write();
+		ArrayList<Score> actual = hsc.read();
+		assertEquals(expected, actual);
 	}
 
 	/**
@@ -74,11 +87,81 @@ public class HighScoreControllerTest {
 		assertEquals(-1, hsc.highScoreIndex(1));
 	}
 	
+	/**
+	 * Test the highScoreIndex method with an empty highscore list.
+	 */
 	@Test
 	public void highScoreIndexTest5() {
 		HighscoreController hsc = new HighscoreController(
 				"src/test/resources/Highscore/highscorestest2.xml");
 		assertEquals(0, hsc.highScoreIndex(1));
 	}
+	
+	/**
+	 * Test the addScore method with an empty highscore list.
+	 */
+	@Test
+	public void addScoreTest1() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest1.xml");
+		
+		ArrayList<Score> expected = new ArrayList<Score>();
+		expected.add(new Score("name", 100));
+		
+		hsc.addScore(new Score("name", 100), 0);
+		ArrayList<Score> actual = hsc.getHighscores();
+		assertEquals(expected, actual);
+	}
 
+	/**
+	 * Test the addScore method with a full highscore list.
+	 */
+	@Test
+	public void addScoreTest2() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest1.xml");
+		
+		ArrayList<Score> expected = new ArrayList<Score>();
+		expected.add(new Score("name", 1000));
+		for (int i = 9; i > 0; i--) {
+			expected.add(new Score(i + "nd", i * 100));
+		}
+		for (int i = 0; i < 10; i++) {
+			hsc.addScore(new Score(9 - i + "nd", (9 - i) * 100), i);
+		}
+		
+		hsc.addScore(new Score("name", 1000), 0);
+		ArrayList<Score> actual = hsc.getHighscores();
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test the addScore method with a too low index.
+	 */
+	@Test
+	public void addScoreTest3() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest1.xml");
+		
+		ArrayList<Score> expected = new ArrayList<Score>();
+		
+		hsc.addScore(new Score("name", 100), -1);
+		ArrayList<Score> actual = hsc.getHighscores();
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Test the addScore method with a too high index.
+	 */
+	@Test
+	public void addScoreTest4() {
+		HighscoreController hsc = new HighscoreController(
+				"src/test/resources/Highscore/highscorestest1.xml");
+		
+		ArrayList<Score> expected = new ArrayList<Score>();
+		
+		hsc.addScore(new Score("name", 100), 2);
+		ArrayList<Score> actual = hsc.getHighscores();
+		assertEquals(expected, actual);
+	}
 }
