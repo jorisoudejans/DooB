f02b8409b544191741607cc3880129453606f349
Shane Koppers <shanekoppers@hotmail.com>
2015-10-20 11:31:54 +0200

    Added tests for the methods spawning balls <testing>

--- src/test/java/doob/model/LevelTest.java
+++ src/test/java/doob/model/LevelTest.java
@@ -3,8 +3,10 @@ package doob.model;
 import javafx.scene.canvas.Canvas;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 
@@ -69,5 +71,52 @@ public class LevelTest {
         players.add(player);
         level.setPlayers(players);
         return level;
+    }
+
+    @Test
+    public void testSpawnSameSizeBalls() {
+        Canvas canvas = new Canvas((double) 1000, (double) 500);
+        Level level = new Level(canvas);
+
+        ArrayList<Ball> balls = new ArrayList<Ball>();
+        level.setBalls(balls);
+        assertEquals(0, level.getBalls().size());
+
+        level.spawnSameSizeBalls(3, 32);
+
+        assertEquals(3, level.getBalls().size());
+        assertEquals((double) 250, level.getBalls().get(0).getX(), 1);
+        assertEquals((double) 500, level.getBalls().get(1).getX(), 1);
+        assertEquals((double) 750, level.getBalls().get(2).getX(), 1);
+
+        assertEquals((double) 125, level.getBalls().get(0).getY(), 1);
+
+        level.getBalls().clear();
+
+        level.spawnSameSizeBalls(2, 64);
+
+        assertEquals(2, level.getBalls().size());
+        assertEquals((double) 333, level.getBalls().get(0).getX(), 1);
+        assertEquals((double) 666, level.getBalls().get(1).getX(), 1);
+
+        assertEquals((double) 125, level.getBalls().get(0).getY(), 1);
+    }
+
+    @Test
+    public void testSpawnBalls(){
+        Level level = new Level(canvas);
+
+        ArrayList<Ball> balls = new ArrayList<Ball>();
+        level.setBalls(balls);
+        assertEquals(0, level.getBalls().size());
+
+        level.spawnBalls(100);
+        assertEquals(2, level.getBalls().size());
+        assertEquals(64, level.getBalls().get(0).getSize());
+
+        level.getBalls().clear();
+        level.spawnBalls(98);
+        assertEquals(3, level.getBalls().size());
+        assertEquals(32, level.getBalls().get(0).getSize());
     }
 }
