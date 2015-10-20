6bb11049b57ed342580ea4b3d9b13db03fc72866
Shane Koppers <shanekoppers@hotmail.com>
2015-10-20 12:21:42 +0200

    Split test methods into smaller ones. <testing>

--- src/test/java/doob/model/LevelTest.java
+++ src/test/java/doob/model/LevelTest.java
@@ -74,49 +74,50 @@ public class LevelTest {
     }
 
     @Test
-    public void testSpawnSameSizeBalls() {
+    public void testSpawnSameSizeBalls1() {
+        Level level = ballsSetup();
+
+        level.spawnSameSizeBalls(3, 32);
+
+        assertEquals((double) 250, level.getBalls().get(0).getX(), 1);
+        assertEquals((double) 750, level.getBalls().get(2).getX(), 1);
+    }
+
+    @Test
+    public void testSpawnSameSizeBalls2() {
+        Level level = ballsSetup();
+
+        level.spawnSameSizeBalls(2, 64);
+
+        assertEquals(2, level.getBalls().size());
+        assertEquals((double) 125, level.getBalls().get(0).getY(), 1);
+    }
+
+    @Test
+    public void testSpawnBalls1(){
+        Level level = ballsSetup();
+
+        level.spawnBalls(98);
+        assertEquals(3, level.getBalls().size());
+        assertEquals(32, level.getBalls().get(0).getSize());
+    }
+
+    @Test
+    public void testSpawnBalls2() {
+        Level level = ballsSetup();
+
+        level.spawnBalls(100);
+        assertEquals(2, level.getBalls().size());
+        assertEquals(64, level.getBalls().get(0).getSize());
+    }
+
+    public Level ballsSetup(){
         Canvas canvas = new Canvas((double) 1000, (double) 500);
         Level level = new Level(canvas);
 
         ArrayList<Ball> balls = new ArrayList<Ball>();
         level.setBalls(balls);
-        assertEquals(0, level.getBalls().size());
 
-        level.spawnSameSizeBalls(3, 32);
-
-        assertEquals(3, level.getBalls().size());
-        assertEquals((double) 250, level.getBalls().get(0).getX(), 1);
-        assertEquals((double) 500, level.getBalls().get(1).getX(), 1);
-        assertEquals((double) 750, level.getBalls().get(2).getX(), 1);
-
-        assertEquals((double) 125, level.getBalls().get(0).getY(), 1);
-
-        level.getBalls().clear();
-
-        level.spawnSameSizeBalls(2, 64);
-
-        assertEquals(2, level.getBalls().size());
-        assertEquals((double) 333, level.getBalls().get(0).getX(), 1);
-        assertEquals((double) 666, level.getBalls().get(1).getX(), 1);
-
-        assertEquals((double) 125, level.getBalls().get(0).getY(), 1);
-    }
-
-    @Test
-    public void testSpawnBalls(){
-        Level level = new Level(canvas);
-
-        ArrayList<Ball> balls = new ArrayList<Ball>();
-        level.setBalls(balls);
-        assertEquals(0, level.getBalls().size());
-
-        level.spawnBalls(100);
-        assertEquals(2, level.getBalls().size());
-        assertEquals(64, level.getBalls().get(0).getSize());
-
-        level.getBalls().clear();
-        level.spawnBalls(98);
-        assertEquals(3, level.getBalls().size());
-        assertEquals(32, level.getBalls().get(0).getSize());
+        return level;
     }
 }
