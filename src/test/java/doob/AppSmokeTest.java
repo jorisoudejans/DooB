2535c7885a1641031e6a0ade5fb2e6b867f9dbc3
Hidde Lycklama <hidde@vlinderstorm.com>
2015-10-30 18:18:52 +0100

    Add tests for every menu option

--- src/test/java/doob/AppSmokeTest.java
+++ src/test/java/doob/AppSmokeTest.java
@@ -4,7 +4,8 @@ import com.sun.javafx.robot.FXRobot;
 import com.sun.javafx.robot.FXRobotFactory;
 import doob.controller.LevelController;
 import doob.controller.MenuController;
-import doob.game.SinglePlayerGame;
+import doob.game.GameUI;
+import doob.game.model.SinglePlayerGame;
 import doob.model.Player;
 import doob.model.level.Level;
 import javafx.application.Application;
@@ -22,6 +23,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.awt.*;
+import java.io.IOException;
 
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assume.assumeNotNull;
@@ -49,46 +51,88 @@ public class AppSmokeTest {
                 App.launch(App.class);
             }
         }).start();
-        Thread.sleep(1000);
+        Thread.sleep(1500);
     }
 
     @Before
     public void setUp() throws Exception {
-        fxmlLoader = new FXMLLoader();
     }
 
     @Test
     public void testLoadMenu() throws Exception {
-
-        System.out.println("Smoking...");
-
-        Pane p = fxmlLoader.load(App.class.getResource("/FXML/Menu.fxml").openStream());
-        MenuController menuController = (MenuController) fxmlLoader.getController();
-
+        MenuController menuController = loadMenu();
         assertTrue(menuController instanceof MenuController);
     }
 
     @Test
     public void testSinglePlayerGame() throws Exception{
-
-        Pane p = fxmlLoader.load(App.class.getResource("/FXML/Menu.fxml").openStream());
-        MenuController menuController = (MenuController) fxmlLoader.getController();
+        MenuController menuController = loadMenu();
         menuController.playSinglePlayer();
+    }
 
-        Thread.sleep(500);
+    @Test
+    public void testCoopGame() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.playCoopMode();
+    }
 
-        FXMLLoader gameLoader = new FXMLLoader();
-        AnchorPane pane = gameLoader.load(App.class.getResource("/FXML/SinglePlayerGame.fxml").openStream());
-        SinglePlayerGame gameController = (SinglePlayerGame) gameLoader.getController();
+    @Test
+    public void testDuelGame() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.playDuelMode();
+    }
 
-        Thread.sleep(500);
+    @Test
+    public void testSurvivalGame() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.playSurvivalMode();
+    }
 
-        Level level = gameController.getLevel();
-        Player p1 = level.getPlayers().get(0);
-        p1.setMoveSpeed(10);
+    @Test
+    public void testOptions() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.showOptions();
+    }
 
-        for (int i = 0; i < 100; i++) {
-            p1.move();
-        }
+    @Test
+    public void testBuildLevel() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.buildLevel();
+    }
+
+    @Test
+    public void testHighscoreSingle() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.showSinglePlayerHighscores();
+    }
+
+    @Test
+    public void testHighscoreDuel() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.showDuelPlayerHighscores();
+    }
+
+    @Test
+    public void testHighscoreCoop() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.showCoopPlayerHighscores();
+    }
+
+    @Test
+    public void testHighscoreSurvival() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.showSurvivalHighscores();
+    }
+
+    @Test
+    public void testMenuBack() throws Exception{
+        MenuController menuController = loadMenu();
+        menuController.backToMenu();
+    }
+
+    private MenuController loadMenu() throws IOException, InterruptedException {
+        fxmlLoader = new FXMLLoader();
+        fxmlLoader.load(App.class.getResource("/FXML/Menu.fxml").openStream());
+        return (MenuController)fxmlLoader.getController();
     }
 }
